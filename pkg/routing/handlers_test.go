c1f587c5e464bf03f9946b2d991e89ee0c568afe
Daniel Arlitt <daniel.arlitt@ticketmaster.com>
2016-06-06 12:57:25 -0700

    Allow setting API key from env var

--- pkg/routing/handlers_test.go
+++ pkg/routing/handlers_test.go
@@ -3,17 +3,39 @@ package routing
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/dgarlitt/urban-gopher/pkg/types"
 	"github.com/dgarlitt/urban-gopher/version"
-	"github.com/stretchr/testify/assert"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestIndexHandlerResponse(t *testing.T) {
-	assert := assert.New(t)
+var router *mux.Router
+
+type FakeDefinitionProvider struct{}
+
+// LookupDefinition - test method for looking up a definition
+func (provider *FakeDefinitionProvider) LookupDefinition(params *types.ProviderParams) (definition *types.Definition, err error) {
+	term := params.Term
+	apikey := params.APIKey
+	if term == "Bob Dole" {
+		definition = &types.Definition{Word: term, Text: "APIKey is " + apikey}
+	} else {
+		err = errors.New("Error: Term = " + term + ", APIKey = " + apikey)
+	}
+
+	return
+}
+
+type HandlersTestSuite struct {
+	suite.Suite
+}
+
+func (suite *HandlersTestSuite) TestIndexHandlerResponse() {
 	request, _ := http.NewRequest("GET", "/", nil)
 	response := httptest.NewRecorder()
 
@@ -34,41 +56,43 @@ func TestIndexHandlerResponse(t *testing.T) {
 
 	healthHandler(response, request)
 
-	assert.Equal(expectedResponseCode, response.Code)
-	assert.Equal(expectedResponseHead, response.HeaderMap.Get("Content-Type"))
-	assert.Equal(string(expectedResponseBody), response.Body.String())
+	suite.Equal(expectedResponseCode, response.Code)
+	suite.Equal(expectedResponseHead, response.HeaderMap.Get("Content-Type"))
+	suite.Equal(string(expectedResponseBody), response.Body.String())
 }
 
-type FakeDefinitionProvider struct{}
-
-// LookupDefinition - test method for looking up a definition
-func (provider *FakeDefinitionProvider) LookupDefinition(params *types.ProviderParams) (definition *types.Definition, err error) {
-	term := params.Term
-	apikey := params.APIKey
-	if term == "Bob Dole" {
-		definition = &types.Definition{Word: term, Text: "APIKey is " + apikey}
-	} else {
-		err = errors.New("Error: Term = " + term + ", APIKey = " + apikey)
-	}
-
-	return
-}
-
-func TestDictionaryHandlerResponse(t *testing.T) {
-	assert := assert.New(t)
+func (suite *HandlersTestSuite) TestDictionaryHandlerResponse() {
 	request, _ := http.NewRequest("GET", "/define?term=Bob+Dole", nil)
 	request.Header.Set("X-API-Key", "bobloblaw")
 	response := httptest.NewRecorder()
 
-	expectedResponseCode := 200
+	expectedResponseCode := http.StatusOK
 	expectedResponseHead := "application/json"
-	expectedResponseBody := `{"word":"Bob Dole","definition":"APIKey is bobloblaw"}`
+	expectedResponseBody := fmt.Sprintf(
+		`{"word":"%s","definition":"%s"}`,
+		"Bob Dole",
+		"APIKey is bobloblaw")
 
-	DefinitionHandlerProvider = &DefinitionHandlerType{Provider: &FakeDefinitionProvider{}}
-	router := SetupRoutes()
 	router.ServeHTTP(response, request)
 
-	assert.Equal(expectedResponseCode, response.Code)
-	assert.Equal(expectedResponseHead, response.HeaderMap.Get("Content-Type"))
-	assert.Equal(expectedResponseBody, response.Body.String())
+	suite.Equal(expectedResponseCode, response.Code)
+	suite.Equal(expectedResponseHead, response.HeaderMap.Get("Content-Type"))
+	suite.Equal(expectedResponseBody, response.Body.String())
+}
+
+func (suite *HandlersTestSuite) TestDictionaryHandlerResponseWithoutEnvAPIKey() {
+	request, _ := http.NewRequest("GET", "/define?term=Bob+Dole", nil)
+	response := httptest.NewRecorder()
+
+	expectedResponseCode := http.StatusBadRequest
+
+	router.ServeHTTP(response, request)
+
+	suite.Equal(expectedResponseCode, response.Code)
+}
+
+func TestHandlersTestSuite(t *testing.T) {
+	DefinitionHandlerProvider = &DefinitionHandlerType{Provider: &FakeDefinitionProvider{}}
+	router = SetupRoutes()
+	suite.Run(t, new(HandlersTestSuite))
 }
